4672011e6e33caaccd4a19475ea2a203316abc84
jnguyen38 <jnguyen8831@gmail.com>
2022-10-21 21:30:07 -0400

    Header development

--- src/js/Header.js
+++ src/js/Header.js
@@ -1,22 +1,40 @@
 import {Link} from "react-router-dom";
+import {useState} from "react";
+import logo from "../media/sliceLogoTransparent.png";
 
 function Header() {
-    const homeRedirect = "./"
-    const regRedirect = "./register"
+    const [menuClicked, setMenuClicked] = useState(false);
+
+    const homeRedirect = "./";
+    const regRedirect = "./register";
+    const contactRedirect = "./contact";
+
+    function menuClick() {
+        setMenuClicked(() => {
+           return !menuClicked;
+        });
+    }
 
     return (
         <div className="header">
-            <section id="header-main">
+            <Link to={homeRedirect} id="header-main" className="d-flex-cc">
+                <img src={logo} alt="" id="header-logo"/>
                 <h1>Slice of Life</h1>
-            </section>
+            </Link>
             <nav id="header-nav" className="d-flex-cc">
-                <div className="nav-item d-flex-cc">
-                    <Link to={homeRedirect}><h2 className="nav-link">Home</h2></Link>
-                </div>
-                <div className="nav-item d-flex-cc">
-                    <Link to={regRedirect}><h2 className="nav-link">Register</h2></Link>
-                </div>
+                <Link to={homeRedirect}>
+                    <div className="nav-item d-flex-cc"><h2 className="nav-link">Home</h2></div>
+                </Link>
+                <Link to={regRedirect}>
+                    <div className="nav-item d-flex-cc"><h2 className="nav-link">Register</h2></div>
+                </Link>
+                <Link to={contactRedirect}>
+                    <div className="nav-item d-flex-cc"><h2 className="nav-link">Contact</h2></div>
+                </Link>
             </nav>
+            <div id="nav-menu" className={(menuClicked) ? "open" : ""} onClick={menuClick}>
+                <span/> <span/> <span/> <span/>
+            </div>
         </div>
     );
 }
