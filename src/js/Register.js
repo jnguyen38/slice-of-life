9167ef666f98708e679f2e7e184522091da4de57
jnguyen38 <jnguyen8831@gmail.com>
2022-10-23 01:04:38 -0400

    Register updates and favicon

--- src/js/Register.js
+++ src/js/Register.js
@@ -4,7 +4,7 @@ function Register() {
             <div id="reg-display">
                 <h1>Registration Form</h1>
                 <form id="reg-form">
-                    <label htmlFor="firstname">First Name</label>
+                    <label htmlFor="firstname" className="reg-label">First Name</label>
                     <input type="text" name="firstname" className="reg-text grid-4"/>
                 </form>
             </div>
