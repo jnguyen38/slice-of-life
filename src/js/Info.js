8aece9b4d6d20869549bb2ef102ad326421d0e75
jnguyen38 <jnguyen8831@gmail.com>
2022-10-25 14:03:25 -0400

    Minor updates to info and register

--- src/js/Info.js
+++ src/js/Info.js
@@ -25,7 +25,7 @@ function InfoDisplay(props) {
             </div>
             <img src="" alt="" className={(props.width > 1000) ? "grid-6" : "grid-12"}/>
             <img src="" alt="" className={(props.width > 1100) ? "grid-4" : "grid-12"}/>
-            <div className={(props.width > 1100) ? "info-text text-left grid-8" : " info-text text-left grid-12"}>
+            <div className={(props.width > 1100) ? "info-text text-right grid-8" : " info-text text-left grid-12"}>
                 <h2>Our History</h2>
                 <p>Slice of Life was founded in 1988 by Ms. Geraldine Griffin. It emerged as one of the leading tutoring organizations in our community. Over the years the program and its participants have been at the forefront of the educational movement dedicated to educating, inspiring and assisting youth of diverse cultures to improve their educational outcomes. Our desire is to empower and support youth as they endeavor to succeed in their personal and academic lives. By participating in the structured and hands-on learning activities provided by Slice of Life, youth develop healthy self-esteem, positive self- image, enduring self-confidence and a genuine respect for themselves and others. This enables them to make positive contributions to their communities. The University of Notre Dame provides students to add the components of tutoring and mentoring. In recent years the students have been a recognized campus group who consistently serve in various aspects of the organization. They effectively recruit students on campus and are responsible for significant amount of fundraising. Notre Dame students make a four year commitment to maximize the benefits of the tutor/student relationship. Tutors also focus on state testing to aid in reading comprehension, fluency and math.</p>
             </div>
@@ -34,7 +34,7 @@ function InfoDisplay(props) {
                 <p>Slice of Life is a development program that provides tutoring and life skill services for only $100 a semester per student. We provide help for children of grades K-5. The program currently operates Tuesday, Wednesday, and Thursday from 3:15PM until 5:15PM. This after school program is located at Christian Life Center South Bend, 1717 N. Hickory Rd., South Bend, IN 46635. We provide individual tutoring, life skills, character development, recreational team building, field trips, guest speakers, small group interactions, nutritional snacks, parent meetings and award ceremony with celebrations.</p>
             </div>
             <img src="" alt="" className={(props.width > 1000) ? "grid-4" : "grid-12"}/>
-            <div className="info-text text-center grid-12" id="info-mission">
+            <div className={(props.width > 1000) ? "info-text text-center grid-8" : "info-text text-center grid-12"} id="info-mission">
                 <h2>Our Mission</h2>
                 <div className="line"/>
                 <p>To provide positive reinforcements for physical and emotional wellness, academic achievement, leadership development and community service to youth in grades K-5.</p>
--- src/js/Register.js
+++ src/js/Register.js
@@ -10,9 +10,13 @@ function Register() {
         <div id="reg-container">
             <div id="reg-display">
                 <h1>Registration Form</h1>
+                <div className="line black"/>
                 <form id="reg-form">
                     <label htmlFor="firstname" className="reg-label">First Name</label>
                     <input type="text" name="firstname" className="reg-text grid-4"/>
+
+                    <label htmlFor="lastname" className="reg-label">Last Name</label>
+                    <input type="text" name="lastname" className="reg-text grid-4"/>
                 </form>
             </div>
         </div>
