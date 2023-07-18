87209490c707d0416975477de98164da38f406bc
jnguyen38 <jnguyen8831@gmail.com>
2023-07-18 01:54:28 -0400

    Header design and functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,7 @@ import logo from "../../public/media/sliceLogoTransparent.png";
 import Link from "next/link";
 import Image from "next/image";
 
-function handleScroll() {
-
-}
-
-function Header() {
+function Header(props) {
     const [menu, setMenu] = useState(false);
 
     const homeRedirect = "/";
@@ -19,30 +15,31 @@ function Header() {
         setMenu(curr => !curr);
     }
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll, { passive: true })
-        return () => {
-            window.removeEventListener('scroll', handleScroll)
-        };
-    });
-
     return (
-        <div className={`${styles.header} d-flex jc-sb`}>
-            <Link href={homeRedirect} className={`${styles.headerMain} d-flex-row-c`}>
-                <Image src={logo} alt={``} width={40} height={40}/>
-                <h1 className={`fs-md fw-2 px-1`}>Slice of Life</h1>
+        <div className={`${styles.header} ${props.scrollingUp ? styles.full : styles.shrink} gap-5 d-flex`}>
+            <Link href={homeRedirect} className={`${styles.headerMain} d-flex`}>
+                <Image src={logo} alt={``} width={props.scrollingUp ? 40:30} height={props.scrollingUp ? 40:30} className={styles.logo}/>
+                <h1 className={`${styles.headerTitle} ${props.scrollingUp ? "fs-smd" : "fs-sm"} fw-2 px-1`}>Slice of Life</h1>
             </Link>
-            <nav className={`${styles.headerNav} gap-1 d-flex-row-c mr-4`}>
+
+            <nav className={`${styles.headerNav} ${props.scrollingUp ? styles.visible : styles.invisible} gap-1 d-flex`}>
                 <Link href={homeRedirect}>
-                    <div className={`${styles.navItem} d-flex-row-c p-1 rounded`}><p className={`fs-sm fw-3`}>Home</p></div>
+                    <div
+                        className={`${styles.navItem} ${props.page === "/" ? styles.selected : ""} d-flex-row-c p-1`}>
+                        <p className={`fs-eh fw-2`}>Home</p>
+                    </div>
                 </Link>
                 <Link href={contactRedirect}>
-                    <div className={`${styles.navItem} d-flex-row-c p-1 rounded`}><p className={`fs-sm fw-3`}>Contact</p></div>
+                    <div
+                        className={`${styles.navItem} ${props.page === "/contact" ? styles.selected : ""} d-flex-row-c p-1`}>
+                        <p className={`fs-eh fw-2`}>Contact</p>
+                    </div>
                 </Link>
-                <Link href={regRedirect}>
-                    <div className={`${styles.navItem} d-flex-row-c p-1 rounded`}><p className={`fs-sm fw-3`}>Register</p></div>
+                <Link href={regRedirect} id={styles['register']} className={`${styles.primaryButton}`}>
+                    Register
                 </Link>
             </nav>
+
             <div id={`menu`} className={`${styles.menu} ${menu ? styles.rotated : ""} d-flex-col-c gap-1 clickable`} onClick={toggleMenu}>
                 <span/><span/><span/>
             </div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,49 @@ import '../css/root.css'
 import { Analytics } from '@vercel/analytics/react';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import {useEffect, useState} from "react";
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps }) {
+    const [page, setPage] = useState("/");
+    const [scrollingUp, setScrollingUp] = useState(true);
+
+    useEffect(() => {
+        const threshold = 0;
+        let lastScrollY = window.scrollY;
+        let ticking = false;
+
+        const updateScrollDir = () => {
+            const scrollY = window.scrollY;
+
+            if (Math.abs(scrollY - lastScrollY) < threshold) {
+                ticking = false;
+                return;
+            }
+            setScrollingUp(scrollY < lastScrollY);
+            lastScrollY = scrollY > 0 ? scrollY : 0;
+            ticking = false;
+        };
+
+        const onScroll = () => {
+            if (!ticking) {
+                window.requestAnimationFrame(updateScrollDir);
+                ticking = true;
+            }
+        };
+
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [scrollingUp]);
+
+    useEffect(() => {
+        if (window.location.pathname)
+            setPage(window.location.pathname)
+    })
+
     return (
         <div>
-            <Header/>
+            <Header page={page} setPage={setPage} scrollingUp={scrollingUp}/>
             <Component {...pageProps}/>
             <Footer/>
             <Analytics/>
@@ -14,3 +52,4 @@ export default function App({ Component, pageProps }) {
     )
 }
 
+export default App
